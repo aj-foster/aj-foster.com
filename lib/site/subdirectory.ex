2ada9a2d18538f284a2a3c10fcdb271c2d6236be
AJ Foster <web@aj-foster.com>
2021-01-14 23:28:12 -0500

    Update page locations and URLs to use subdirectories

--- lib/site/subdirectory.ex
+++ lib/site/subdirectory.ex
@@ -2,10 +2,12 @@ defmodule Site.Subdirectory do
   @moduledoc """
   Serum Plugin that transforms post output paths and URLs so that instead of:
 
+      /about.html
       /posts/yyyy-mm-dd-post-title.html
 
   we get:
 
+      /about/[index.html]
       /yyyy/post-title/[index.html]
 
   This URL scheme aligns with previous iterations of the site, to which there may be external links.
@@ -17,7 +19,58 @@ defmodule Site.Subdirectory do
   def elixir, do: "~> 1.11"
   def serum, do: "~> 1.5"
   def description, do: "Make each post a subdirectory instead of a .html file"
-  def implements, do: [processed_post: 2]
+  def implements, do: [processed_page: 2, processed_post: 2]
+
+  @doc """
+  Alter the output file and URL of a processed page to use the desired scheme.
+  """
+  @spec processed_page(Serum.Page.t(), any) :: {:ok, Serum.Page.t()}
+  def processed_page(page, _args) do
+    case Regex.run(~r/\/(.*)\.md$/, page.file) |> IO.inspect() do
+      [_full_match, "index"] ->
+        {:ok, page}
+
+      [_full_match, _name] ->
+        page =
+          Map.update!(page, :output, &update_page_path/1)
+          |> Map.update!(:url, &update_page_url/1)
+          |> IO.inspect()
+
+        {:ok, page}
+
+      _ ->
+        {:ok, page}
+    end
+  end
+
+  # Transform /path/to/page.html
+  # to        /path/to/page/index.html
+  #
+  @spec update_page_path(String.t()) :: String.t()
+  defp update_page_path(path) do
+    first = Path.dirname(path)
+
+    second =
+      Path.basename(path)
+      |> String.trim_trailing(".html")
+      |> Path.join("/index.html")
+
+    Path.join(first, second)
+  end
+
+  # Transform /page.md
+  # to        /page/
+  #
+  @spec update_page_url(String.t()) :: String.t()
+  defp update_page_url(path) do
+    first = Path.dirname(path)
+
+    second =
+      Path.basename(path)
+      |> String.trim_trailing(".html")
+
+    Path.join(first, second) <> "/"
+  end
 
   @doc """
   Alter the output file and URL of a processed post to use the desired scheme.
